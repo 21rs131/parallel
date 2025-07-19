afec628d6499ff053a9801d3436d34294e9062ce
Ryotaro MORIWAKI <taro@smkwlab.net>
2025-07-19 15:40:39 +0900

    逐次処理のプログラムを追加

---
+++ src/serial.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import time
+
+def count_in_circle(n):
+    cnt = 0
+    for _ in range(n):
+        x, y = random.random(), random.random()
+        if x*x + y*y <= 1:
+            cnt += 1
+    return cnt
+
+def main(total_cnt):
+    #print(f"Starting serial Monte Carlo π estimation with {total_cnt} samples")
+    start = time.time()
+    total_in_circle = count_in_circle(total_cnt)
+    pi = 4 * total_in_circle / total_cnt
+    elapsed = time.time() - start
+
+    print(f"Estimated π = {pi}")
+    print(f"Total elapsed time: {elapsed:.9f} seconds")
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        total = int(sys.argv[1])
+    else:
+        total = 1_000_000_000
+    main(total)
